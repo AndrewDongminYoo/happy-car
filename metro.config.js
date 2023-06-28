89c0b213a87fd79ac88526644a04aa70edd08846
Dongmin,Yoo <ydm2790@gmail.com>
2023-06-29 00:37:58 +0900

    build(deps): 👷 바벨, 메트로, 타입스크립트 설정 업데이트

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,22 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * @format
  *
+ * Metro configuration
+ * @see https://facebook.github.io/metro/docs/configuration
+ * @see https://github.com/facebook/react-native
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
